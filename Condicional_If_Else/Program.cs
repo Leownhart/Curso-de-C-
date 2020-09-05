2bb22a3e05a714bde71386ba42eee2d936154a46
Leownhart <francisco.amartins.al@gmail.com>
2020-09-05 18:44:20 -0300

    Condicional If

---
+++ Condicional_If_Else/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Condicional_If_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            Console.WriteLine("Bom dia");
+            if (x < 5)
+            {
+                Console.WriteLine("Boa Tarde");
+            }
+            Console.WriteLine("Boa Noite");
+
+            Console.WriteLine("Entre com um número inteiro: ");
+            int y = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Par!");
+            }
+            else
+                Console.WriteLine("Impar!");
+        }
+    }
+}
