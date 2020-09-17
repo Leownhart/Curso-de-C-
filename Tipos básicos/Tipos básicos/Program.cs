9b8ce01efcf6179333739425f5e084c366d28ae5
Francisco Leandro Almeida Martins <francisco.amartins.al@gmail.com>
2020-09-17 16:32:11 -0300

    Tipos Básicos

---
+++ Tipos básicos/Tipos básicos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tipos_básicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*byte N1 = 254;
+
+            N1++;
+            Console.WriteLine(N1);*/
+
+            sbyte Y = 127;
+            int n2 = 1000;
+            Console.WriteLine(Y);
+            int n3 = 2147483647;
+            long n4 = 2147483647L;
+
+            Console.WriteLine(Y);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+
+        }
+    }
+}
