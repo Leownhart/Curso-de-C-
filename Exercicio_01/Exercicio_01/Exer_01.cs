93106338c5aab0499eb7ded986ebe211a4b9b86d
Francisco Leandro Almeida Martins <francisco.amartins.al@gmail.com>
2020-09-18 20:18:40 -0300

    Update Exer_01.cs

--- Exercicio_01/Exercicio_01/Exer_01.cs
+++ Exercicio_01/Exercicio_01/Exer_01.cs
@@ -7,7 +7,7 @@ namespace Exercicios_Fix
         static void Main(string[] args)
         {
             /* Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma
-             * dessess valores com uma mesagem explicativa.*/
+             * desses valores com uma mesagem explicativa.*/
 
             int n1, n2, n3, soma;
             Console.WriteLine("Informe um número: ");
